0ae234ce01b8d5e600dff32a547b7a8bde4c729e
Sumit-zignex <sumit@zignex.com>
2024-11-07 13:53:24 +0530

    restructed and deleted unused files

--- karma.conf.js
+++ karma.conf.js
@@ -1,50 +1,32 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const { join } = require('path');
-const { constants } = require('karma');
-
-module.exports = () => {
-  return {
+module.exports = function (config) {
+  config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-      jasmine: {
-        random: true
-      }
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: join(__dirname, 'coverage'),
-      reports: ['text-summary', 'html', 'lcovonly'],
-      fixWebpackSourcePaths: true,
-      thresholds: {
-        emitWarning: false, // <- this is important to make karma fail
-        global: {
-          statements: 0,
-          lines: 0,
-          branches: 0,
-          functions: 0
-        }
-      }
+      dir: require('path').join(__dirname, '../../coverage/angular-tree-component'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
     },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
-    browserNoActivityTimeout: 1000000,
     colors: true,
-    logLevel: constants.LOG_INFO,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
-  };
+    singleRun: false,
+    restartOnFileChange: true
+  });
 };
